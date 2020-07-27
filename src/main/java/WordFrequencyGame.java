3dd51a168f0b2170f2ecdb66bd6f2478c41920f2
Bruno8310 <gongl3730@gmail.com>
2020-07-27 22:19:34 +0800

    [WorldFrequencyGame][Bruno] feature: add calculteWorldFrequency and

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -10,60 +10,34 @@ public class WordFrequencyGame {
 
     private final String SPACE = " ";
 
-
     public String getResult(String inputStr) {
+        try {
+            List<WordInfo> inputList = calculteWorldFrequency(inputStr);
 
-        if (inputStr.split(SPLIT_PATTERN).length==1) {
-            return inputStr + " 1";
-        } else {
-            try {
-                List<WordInfo> inputList = calculteWorldFrequency(inputStr);
+            inputList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
 
-                for (String word : words) {
-                    WordInfo wordInfo = new WordInfo(word, 1);
-                    inputList.add(wordInfo);
-                }
+            return generateWorldFrequenceResult(inputList);
 
-                Map<String, List<WordInfo>> wordInfosMap = getListMap(inputList);
-
-                List<WordInfo> wordInfos = new ArrayList<>();
-
-
-                for (Map.Entry<String, List<WordInfo>> entry : wordInfosMap.entrySet()) {
-                    WordInfo input = new WordInfo(entry.getKey(), entry.getValue().size());
-                    wordInfos.add(input);
-                }
-                inputList = wordInfos;
-
-                inputList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
-
-                StringJoiner joiner = new StringJoiner(LINE);
-                for (WordInfo w : inputList) {
-                    String s = w.getValue() + SPACE + w.getWordCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-                return CALCULATE_MESSAGE;
-            }
+        } catch (Exception e) {
+            return CALCULATE_MESSAGE;
         }
-    }
+        }
 
     private List<WordInfo> calculteWorldFrequency(String inputStr) {
-        return null;
+        List<WordInfo> wordInfos = new ArrayList<>();
+        List<String> words = Arrays.asList(inputStr.split(SPLIT_PATTERN));
+        for (String unitWord : new HashSet<>(words)) {
+
+        }
+        return wordInfos;
     }
 
-    private Map<String, List<WordInfo>> getListMap(List<WordInfo> inputList) {
-        Map<String, List<WordInfo>> wordInfosMap = new HashMap<>();
-        for (WordInfo input : inputList) {
-            if (!wordInfosMap.containsKey(input.getValue())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(input);
-                wordInfosMap.put(input.getValue(), arr);
-            } else {
-                wordInfosMap.get(input.getValue()).add(input);
-            }
+    private String generateWorldFrequenceResult(List<WordInfo>  inputList) {
+        StringJoiner joiner = new StringJoiner(LINE);
+        for (WordInfo w : inputList) {
+            String s = w.getValue() + SPACE + w.getWordCount();
+            joiner.add(s);
         }
-        return wordInfosMap;
+        return joiner.toString();
     }
 }
